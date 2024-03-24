6f243a25d416541f56b1bafc4839275ee2c5abff
chefe <hi@chefe.io>
2024-03-24 11:20:21 +0100

    add support for language specific templates

--- state/state.go
+++ state/state.go
@@ -1,6 +1,8 @@
 package state
 
 import (
+	"fmt"
+	"regexp"
 	"strconv"
 	"strings"
 	"time"
@@ -16,11 +18,18 @@ type documentInfo struct {
 type State struct {
 	// Map file names to document infos
 	Documents map[string]documentInfo
+	// Map languages to template lines
+	Templates map[string][]string
 }
 
 func NewState() State {
+	templates := map[string][]string{}
+	templates["markdown"] = []string{"<!-- Copyright (C) {year} MARKDOWN -->"}
+	templates["sh"] = []string{"# Copyright (C) {year} SHELLSCRIPT"}
+
 	return State{
 		Documents: map[string]documentInfo{},
+		Templates: templates,
 	}
 }
 
@@ -47,9 +56,43 @@ func (s *State) CloseDocument(document string) {
 	delete(s.Documents, document)
 }
 
-func buildCopyrightLine(template string) string {
+func matchesTemplateLine(line string, template string) bool {
+	pattern := fmt.Sprintf("^%s$", regexp.QuoteMeta(template))
+	pattern = strings.ReplaceAll(pattern, "\\{year\\}", "[0-9]{4}")
+	return regexp.MustCompile(pattern).Match([]byte(line))
+}
+
+func containsCopyrightString(lines []string, template []string) bool {
+	if len(template) == 0 {
+		return false
+	}
+
+	for lineIndex, line := range lines {
+		if !matchesTemplateLine(line, template[0]) {
+			continue
+		}
+
+		if lineIndex+len(template) > len(lines) {
+			// not enought lines left to match template
+			return false
+		}
+
+		for templateIndex, templateLine := range template[1:] {
+			if !matchesTemplateLine(lines[lineIndex+templateIndex+1], templateLine) {
+				return false
+			}
+		}
+
+		return true
+	}
+
+	return false
+}
+
+func buildCopyrightString(template []string) string {
 	year := strconv.Itoa(time.Now().Year())
-	return strings.ReplaceAll(template, "{year}", year)
+	content := strings.Join(template, "\n")
+	return strings.ReplaceAll(content, "{year}", year)
 }
 
 func (s *State) CalculateCodeActions(document string, start lsp.Position, end lsp.Position) []lsp.CodeAction {
@@ -58,19 +101,24 @@ func (s *State) CalculateCodeActions(document string, start lsp.Position, end ls
 		return []lsp.CodeAction{}
 	}
 
+	templateLines, ok := s.Templates[doc.Language]
+	if !ok {
+		return []lsp.CodeAction{}
+	}
+
 	lines := strings.Split(doc.Content, "\n")
 	actions := []lsp.CodeAction{}
 
-	limit := 10
+	limit := 10 + len(templateLines)
 	if len(lines) < limit {
 		limit = len(lines)
 	}
 
-	if !strings.Contains(strings.Join(lines[:limit], "\n"), "Copyright") {
+	if !containsCopyrightString(lines[:limit], templateLines) {
 		changes := map[string][]lsp.TextEdit{}
 		changes[document] = []lsp.TextEdit{
 			{
-				NewText: buildCopyrightLine("// Copyright (C) {year} AUTHOR\n"),
+				NewText: fmt.Sprintf("%s\n", buildCopyrightString(templateLines)),
 				Range:   lsp.NewRange(0, 0, 0, 0),
 			},
 		}
---
+++ state/state_test.go
@@ -0,0 +1,149 @@
+package state
+
+import "testing"
+
+func TestMatchesTemplateLine(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		line     string
+		template string
+		want     bool
+	}{
+		{
+			name:     "empty line and empty template",
+			line:     "",
+			template: "",
+			want:     true,
+		},
+		{
+			name:     "different line and template",
+			line:     "foo",
+			template: "bar",
+			want:     false,
+		},
+		{
+			name:     "matches template with year placeholder",
+			line:     "# Copyright (C) 2024 AUTHOR",
+			template: "# Copyright (C) {year} AUTHOR",
+			want:     true,
+		},
+		{
+			name:     "year is not a number",
+			line:     "# Copyright (C) asdf AUTHOR",
+			template: "# Copyright (C) {year} AUTHOR",
+			want:     false,
+		},
+		{
+			name:     "line is longer then template",
+			line:     "# Copyright (C) 2024 AND MORE",
+			template: "# Copyright (C) {year}",
+			want:     false,
+		},
+		{
+			name:     "line is shorter then template",
+			line:     "# Copyright (C) 2024",
+			template: "# Copyright (C) {year} AUTHOR",
+			want:     false,
+		},
+		{
+			name:     "template with multiple year placeholder",
+			line:     "# Copyright 2020 (C) 2022 AUTHOR 2024",
+			template: "# Copyright {year} (C) {year} AUTHOR {year}",
+			want:     true,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := matchesTemplateLine(tt.line, tt.template)
+			if got != tt.want {
+				t.Fatalf("expected: %t, got: %t", tt.want, got)
+			}
+		})
+	}
+}
+
+func TestContainsCopyrightString(t *testing.T) {
+	t.Parallel()
+
+	tests := []struct {
+		name     string
+		lines    []string
+		template []string
+		want     bool
+	}{
+		{
+			name:     "nil for lines and template",
+			lines:    nil,
+			template: nil,
+			want:     false,
+		},
+		{
+			name:     "empty slice for line and template",
+			lines:    []string{},
+			template: []string{},
+			want:     false,
+		},
+		{
+			name:     "single line template with one matching line",
+			lines:    []string{"# Copyright (C) 2024 AUTHOR"},
+			template: []string{"# Copyright (C) {year} AUTHOR"},
+			want:     true,
+		},
+		{
+			name:     "single line template with second line matching",
+			lines:    []string{"#!/bin/sh", "# Copyright (C) 2024 AUTHOR"},
+			template: []string{"# Copyright (C) {year} AUTHOR"},
+			want:     true,
+		},
+		{
+			name:     "single line template with no line matching",
+			lines:    []string{"#!/bin/sh", "echo 'test'"},
+			template: []string{"# Copyright (C) {year} AUTHOR"},
+			want:     false,
+		},
+		{
+			name:     "single line template with no lines",
+			lines:    []string{},
+			template: []string{"# Copyright (C) {year} AUTHOR"},
+			want:     false,
+		},
+		{
+			name:     "multi line template with match starting at line zero",
+			lines:    []string{"/*", " * Copyright (C) 2024 AUTHOR", " */"},
+			template: []string{"/*", " * Copyright (C) {year} AUTHOR", " */"},
+			want:     true,
+		},
+		{
+			name:     "multi line template with match starting at another line",
+			lines:    []string{"// Another comment", "", "/*", " * Copyright (C) 2024 AUTHOR", " */"},
+			template: []string{"/*", " * Copyright (C) {year} AUTHOR", " */"},
+			want:     true,
+		},
+		{
+			name:     "multi line template with only some lines matching",
+			lines:    []string{"/*", " * Copyright (C) 2024 AUTHOR"},
+			template: []string{"/*", " * Copyright (C) {year} AUTHOR", " */"},
+			want:     false,
+		},
+	}
+
+	for _, tt := range tests {
+		tt := tt
+
+		t.Run(tt.name, func(t *testing.T) {
+			t.Parallel()
+
+			got := containsCopyrightString(tt.lines, tt.template)
+			if got != tt.want {
+				t.Fatalf("expected: %t, got: %t", tt.want, got)
+			}
+		})
+	}
+}
